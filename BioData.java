7dce9cf26068ba6a40184bdf0b1cc9b9ade0a7f1
BashakTech <chandanbashak1@gmail.com>
2019-02-21 22:29:07 +0600

    my biodata

---
+++ BioData.java
@@ -0,0 +1,15 @@
+
+public class BioData {
+
+	public static void main(String[] args) {
+		
+		// write name in " " double quotetion
+		System.out.println("Name: Chandan Kumar Bashak");
+
+		// write mobile number
+		System.out.println("Mobile: 0123456789");
+
+		// write city
+		System.out.println("City: Dhaka,Bangladesh");
+	}
+}
